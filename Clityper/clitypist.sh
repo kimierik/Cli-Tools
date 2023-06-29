fa6e183248c83b4637b4cda46479c86900172c4a
kimierik <kimi.malkamaki@gmail.com>
2023-06-29 08:35:19 +0300

    can now add multiple entries at a time

--- Clityper/clitypist.sh
+++ Clityper/clitypist.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
 words=$(cat /usr/share/dict/usa | sort -R |head -n5)
+
 clear
 echo "$words"
+#how do we do we read multiple words from the line 
+#we need to split it by whitelint
 
-while read line
+while read lines
 do 
+    for line in $lines;do
 
-    for word in $words; do
-        #echo "$word"
-        if [ $word = $line ]
-        then
-            buffer=()
-            for a in $words; do
-                if [ $word != $a ]; then
-                    buffer+=($a)
+        for word in $words; do
+            #echo "$word"
+            if [ $word = $line ]
+            then
+                buffer=()
+                for a in $words; do
+                    if [ $word != $a ]; then
+                        buffer+=($a)
+                    fi
+                done
+                words=${buffer[@]}
+                
+                if [ ${#buffer[@]} -eq 0 ];then
+                    exit 0
                 fi
-            done
-            words=${buffer[@]}
-            
-            if [ ${#buffer[@]} -eq 0 ];then
-                exit 0
+                clear;
+                for i in "${words[@]}"; do printf "%s \n" $i;done
+     
             fi
-            clear;
-            for i in "${words[@]}"; do printf "%s \n" $i;done
- 
-        fi
+        done
     done
 
 done <  "${1:-/dev/stdin}"
